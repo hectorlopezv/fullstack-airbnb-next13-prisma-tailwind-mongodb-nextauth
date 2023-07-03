a6751cdfbab7e9ac285f400146e19cb094b1d179
hector lopez <hectorvmlopez@gmail.com>
2023-07-03 12:56:20 -0500

    adding session provider

---
+++ app/providers/SessionProvider.tsx
@@ -0,0 +1,10 @@
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function SessionProviderWrapper({ children }: Props) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
