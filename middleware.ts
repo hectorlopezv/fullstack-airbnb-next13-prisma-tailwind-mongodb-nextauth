b254689a40f43decf75a5431006b8620e5ce2149
hector lopez <hectorvmlopez@gmail.com>
2023-07-04 20:42:32 -0500

    adding middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware";
 
-export const config = ["/trips", "/reservations", "/properties", "/favorites"];
+export const config = {
+  matcher: ["/trips", "/reservations", "/properties", "/favorites"],
+};
